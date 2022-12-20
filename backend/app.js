1c413e24cf6e97bb76b74b353c81643cdcd8a3a3
code-today <blogarpit21@gmail.com>
2022-12-21 00:27:52 +0530

    added cors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express");
-const connectToDB = require("./config/db");
-const app = express();
+const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 
-// Middleware
+const connectToDB = require("./config/db");
 
+const app = express();
+
+// Express Middleware
 app.use(express.json()); // this will accept json value send from frontend
 app.use(express.urlencoded({ extended: true })); // for accepting formpurlencoded
+app.use(cors());
 
+// Initialising connection to DB
 connectToDB();
 
 app.use("/", userRoutes);
